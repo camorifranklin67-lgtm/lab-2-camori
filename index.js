d2d90d85c935f4cccb3da04eb97883725bcace18
camorifranklin67-lgtm <camorifranklin67@gmail.com>
2025-11-24 21:40:34 +0000

    Complete Lab 2: file operations and JS scripts

---
+++ index.js
@@ -0,0 +1,18 @@
+// Lab Assignment 2.4 - Main JavaScript file
+
+// 1. Simple addition
+const a = 10;
+const b = 5;
+const sum = a + b;
+console.log("1) Sum of a + b =", sum);
+
+// 2. Area of a rectangle
+const width = 4;
+const height = 3;
+const area = width * height;
+console.log("2) Area of rectangle =", area);
+
+// 3. Temperature conversion: Celsius to Fahrenheit
+const celsius = 25;
+const fahrenheit = celsius * 9 / 5 + 32;
+console.log("3) Temperature in Fahrenheit =", fahrenheit);
